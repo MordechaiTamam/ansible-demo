737c579bf12a5c59e9ad2c43ad1444dac37fc2c1
MordechaiTamam <modi.tamam@gmail.com>
2021-11-09 14:32:31 +0200

    Added documentation

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+IP_ADDR=localhost
+> Containers setup
 docker rm -f ansible_node1  ansible_node2
 docker run -d -p 1022:22 --name ansible_node1 moditamam/ansible-demo
 docker run -d -p 1023:22 --name ansible_node2 moditamam/ansible-demo
 
+# Dynamicaly extracting the the containers ports
 NODE1=`docker port ansible_node1 22 | awk -F ':' '{print $2}'`
+
+sshpass is a utility designed for running ssh using the mode referred to as "keyboard-interactive" password authentication, but in non-interactive mode.
+#
+#ssh uses direct TTY access to make sure that the password is indeed issued by an interactive keyboard user.
+# Sshpass runs ssh in a dedicated tty, fooling it into thinking it is getting the password from an interactive user.
 sshpass -p screencast ssh-copy-id -p $NODE1 root@localhost
 echo "succeeded node1..."
 
@@ -12,7 +20,6 @@ sshpass -p screencast ssh-copy-id -p $NODE2 root@localhost
 echo "succeeded node2..."
 #
 
-IP_ADDR=localhost
 > hosts
 echo '[servers]'  >> hosts
 echo "node1 ansible_host=$IP_ADDR ansible_port=$NODE1" >> hosts
